b803447adc3e39e8c9a4a7b6034e4cc68b558bc5
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-01-01 18:36:04 -0500

    Styled the text for this component

--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -6,23 +6,33 @@ import {
     SliderThumb,
     SliderMark,
     Flex,
-    Box
+    Box, Heading, Text, Spacer
   } from '@chakra-ui/react'
 const SliderComponent = (props) => {
     return (
-        <Box 
+        <Flex 
             w={props.width? props.width : '250px'}
             mt={8}
             mr={props.mr? props.mr : 10}
+            flexDirection='column'
+            alignItems='flex-start'
         >
-            <h2>{props.Placeholder}: <b>{props.stateValue}</b></h2>
+            <Flex
+                alignItems='center'
+                textAlign='center'
+
+            >
+                <Text fontSize='18px' fontWeight='semibold' mr={3}>{props.Placeholder}:</Text>
+                <Text fontSize='20px' fontWeight='bold'>{props.stateValue}</Text>
+            </Flex>
+    
             <Slider  defaultValue={0} min={props.minValue} max={props.maxValue? props.maxValue : 400000} onChange={props.onChangeFunction}>
                 <SliderTrack bg="white" boxSize={1.5}>
                     <SliderFilledTrack bg='purple.300' />
                 </SliderTrack>
                 <SliderThumb boxSize={4} border='4px' borderColor='purple.400' />
             </Slider>
-        </Box>
+        </Flex>
     )
 }
 

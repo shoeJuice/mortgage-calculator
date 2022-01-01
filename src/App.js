b7368df4c96ae30bbc7cb6d646e34ca7f056b597
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-01-01 18:37:13 -0500

    Finished! This is the first version of the mortgage calculator.
    It will need responsiveness edits in the next push.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ChakraProvider, Flex, HStack, Spacer, Button} from '@chakra-ui/react'
+import {ChakraProvider, Flex, HStack, Spacer, Button, Text, Heading} from '@chakra-ui/react'
 import SliderComponent from './components/SliderComponent';
+import TextInputComponent from './components/TextInputComponent';
+import CurrencyDisplayComponent from './components/CurrencyDisplayComponent';
 import React from 'react';
 
 function App() {
 
-  const [numVal, setNumVal] = React.useState(0);
+  const [purchasePrice, setpurchasePrice] = React.useState(0);
   const [downPayment, setDownPayment] = React.useState(0);
   const [repaymentTime, setRepaymentTime] = React.useState(0);
   const [interestRate, setInterestRate] = React.useState(0);
   const [loanAmount, setLoanAmount] = React.useState(0);
   const [prMonth, setPrMonth] = React.useState(0);
+  const [mortgageQuote, setMortgageQuote] = React.useState(0);
+
+
+  var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0
+  })
+
+  const calculateMortgageQuote = () => {
+    var numPayments = (repaymentTime * 12)
+    var principalAmount = (purchasePrice - downPayment)
+    setLoanAmount(principalAmount)
+    return (principalAmount * ( ((interestRate*(1 + interestRate))^numPayments) / ((((1 + interestRate))^numPayments) - 1 )) );
+  }
 
   return (
     <ChakraProvider> 
@@ -20,32 +37,44 @@ function App() {
         justifyContent="center"
         alignItems="center"
         flexDirection="column"
-        
+        height="100vh"
+        bg='#f5f3fb'
       >
         <Flex
           bgColor="purple.50"
           borderRadius={6}
+          borderWidth={10}
+          borderColor="white"
           flexDirection="column"
           padding={10}
           mt={10}
         >
+          <Heading as="h2" size="lg">Mortgage Calculator</Heading>
                 <Flex
                 justifyContent="center"
-                alignItems="center"
+                alignItems="flex-start"
                 flexDirection="row"
                 maxWidth='1400px'
                 flexWrap="wrap"
                 pb={8}
                 
               >
-                <SliderComponent width={'400px'} Placeholder={"Purchase Price"} stateValue={numVal} onChangeFunction={(val) => {setNumVal(val)}} />
-                <SliderComponent width={'400px'} Placeholder={"Down Payment"} stateValue={downPayment} onChangeFunction={(val) => {setDownPayment(val)}} />
-                <SliderComponent width={'400px'} Placeholder={"Repayment Time"} stateValue={repaymentTime} maxValue={25} onChangeFunction={(val) => {setRepaymentTime(val)}} />
-                <SliderComponent width={'400px'} Placeholder={"Interest Rate"} stateValue={interestRate} maxValue={25} onChangeFunction={(val) => {setInterestRate(val)}} />
-                <SliderComponent width={'400px'} Placeholder={"Loan Amount"} stateValue={loanAmount} maxValue={1000000} onChangeFunction={(val) => {setLoanAmount(val)}} />
-                <SliderComponent width={'400px'} Placeholder={"Estimated pr. month"} stateValue={prMonth} maxValue={10000} onChangeFunction={(val) => {setPrMonth(val)}} />
+                <SliderComponent width={'400px'} Placeholder={"Purchase Price"} stateValue={formatter.format(purchasePrice)} onChangeFunction={(val) => {setpurchasePrice(val)}} />
+                <SliderComponent width={'400px'} Placeholder={"Down Payment"} stateValue={formatter.format(downPayment)} onChangeFunction={(val) => {setDownPayment(val)}} />
+                <SliderComponent width={'400px'} Placeholder={"Repayment Time"} stateValue={(repaymentTime == 0) ? `${repaymentTime}` : `${repaymentTime} years`} maxValue={25} onChangeFunction={(val) => {setRepaymentTime(val)}} />
+                <SliderComponent width={'400px'} Placeholder={"Interest Rate"} stateValue={`${interestRate}%`} maxValue={100} onChangeFunction={(val) => {setInterestRate(val)}} />
+                {/* Okay these two components below will take text input and convert it to a numerical value */}
+                <CurrencyDisplayComponent placeholderText={'Loan amount:'} textValue={formatter.format(loanAmount)} />
+                <CurrencyDisplayComponent placeholderText={'Estimated pr month:'} textValue={formatter.format(mortgageQuote)} />
+                
               </Flex>
-              <Button mt={10} padding={10} maxWidth='400px'colorScheme="purple">Get a Mortgage Quote</Button>
+              <Button mt={10} padding={10} maxWidth='400px'colorScheme="purple" onClick={(e) => {
+                setMortgageQuote(calculateMortgageQuote)
+                console.log(calculateMortgageQuote())
+              
+              }}>
+                
+                Get a Mortgage Quote</Button>
         </Flex>
         
         

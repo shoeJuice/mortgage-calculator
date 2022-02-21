be5c85c2aff3f475e3bfd6720e7845fa4364dd0a
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-02-21 13:38:36 -0500

    removed some unused dependencies, ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {ChakraProvider, Flex, HStack, Spacer, Button, Text, Heading} from '@chakra-ui/react'
 import SliderComponent from './components/SliderComponent';
-import TextInputComponent from './components/TextInputComponent';
 import CurrencyDisplayComponent from './components/CurrencyDisplayComponent';
 import React from 'react';
 
@@ -12,7 +10,7 @@ function App() {
   const [purchasePrice, setpurchasePrice] = React.useState(0);
   const [downPayment, setDownPayment] = React.useState(0);
   const [repaymentTime, setRepaymentTime] = React.useState(0);
-  const [interestRate, setInterestRate] = React.useState(0);
+  const [interestRate, setInterestRate] = React.useState(0.00);
   const [loanAmount, setLoanAmount] = React.useState(0);
   const [prMonth, setPrMonth] = React.useState(0);
   const [mortgageQuote, setMortgageQuote] = React.useState(0);
@@ -26,7 +24,7 @@ function App() {
 
   const calculateMortgageQuote = () => {
     const p = purchasePrice - downPayment;
-    const r = (interestRate / 12).toFixed(20);
+    const r = (interestRate / 12)
     const n = repaymentTime * 12;
 
     const mp = p * (r * ((1+r) ^ n) / (((1+r)^n) - 1));
@@ -67,7 +65,7 @@ function App() {
                 <SliderComponent width={'400px'} Placeholder={"Purchase Price"} maxValue={15000000} stateValue={formatter.format(purchasePrice)} onChangeFunction={(val) => {setpurchasePrice(val)}} />
                 <SliderComponent width={'400px'} Placeholder={"Down Payment"} stateValue={formatter.format(downPayment)} onChangeFunction={(val) => {setDownPayment(val)}} />
                 <SliderComponent width={'400px'} Placeholder={"Repayment Time"} stateValue={(repaymentTime === 0) ? `${repaymentTime}` : `${repaymentTime} years`} maxValue={40} onChangeFunction={(val) => {setRepaymentTime(val)}} />
-                <SliderComponent width={'400px'} Placeholder={"Interest Rate"} stateValue={`${interestRate * 100}%`} maxValue={100} onChangeFunction={(val) => {setInterestRate((val / 100))}} />
+                <SliderComponent width={'400px'} Placeholder={"Interest Rate"} stateValue={`${interestRate * 100}%`} maxValue={100} onChangeFunction={(val) => {setInterestRate(parseFloat(val / 100))}} />
                 {/* Okay these two components below will take text input and convert it to a numerical value */}
                 <CurrencyDisplayComponent placeholderText={'Loan amount:'} textValue={formatter.format(loanAmount)} />
                 <CurrencyDisplayComponent placeholderText={'Estimated pr month:'} textValue={formatter.format((mortgageQuote))} />
@@ -75,9 +73,7 @@ function App() {
               </Flex>
               <Button mt={10} padding={10} maxWidth='400px'colorScheme="purple" onClick={(e) => {
                 calculateMortgageQuote()
-              }}>
-                
-                Get a Mortgage Quote</Button>
+              }}>Get a Mortgage Quote</Button>
         </Flex>
         
         
